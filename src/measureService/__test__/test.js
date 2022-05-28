2f4885573bb08f95d808478be633665da4bee718
alan890104 <alan890104@gmail.com>
2022-05-28 14:51:11 +0800

    fix: Need at least one testfile to test
    you can remove test.js anytime

---
+++ src/measureService/__test__/test.js
@@ -0,0 +1,9 @@
+describe("Calculator tests", () => {
+  test("adding 1 + 2 should return 3", () => {
+    // arrange and act
+    var result = 3;
+
+    // assert
+    expect(result).toBe(3);
+  });
+});
