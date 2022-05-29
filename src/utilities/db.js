6bf35b278ac0e4be9e6a7ab6b85a7149840efa4e
Azetry <azetry.bt10@nycu.edu.com>
2022-05-29 23:28:27 +0800

    Add methods of mongoDB and initialize mongoDB connection

---
+++ src/utilities/db.js
@@ -0,0 +1,31 @@
+const config = require('config');
+const mongoose = require('mongoose');
+
+const logger = require('./logger')('MongoDB');
+
+const init = async () => {
+  try {
+    await mongoose.connect(config.mongodb.connection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    logger.info('Successfully connect to MongoDB', { module: 'db', method: 'init' });
+  } catch (err) {
+    logger.error(err.message, { module: 'db', method: 'init' });
+  }
+};
+
+const deinit = async () => {
+  try {
+    await mongoose.disconnect();
+
+    logger.info('Successfully disconnect to MongoDB', { module: 'db', method: 'deinit' });
+  } catch (err) {
+    logger.error(err.message, { module: 'db', method: 'deinit' });
+  }
+};
+
+module.exports = {
+  init,
+  deinit,
+};
