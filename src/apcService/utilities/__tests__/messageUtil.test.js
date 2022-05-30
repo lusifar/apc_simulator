0e9775eec4d713235c33278217e6a8f082fa2422
Azetry <azetry.bt10@nycu.edu.com>
2022-05-31 01:16:16 +0800

    Nothning

--- src/apcService/utilities/__tests__/messageUtil.test.js
+++ src/apcService/utilities/__tests__/messageUtil.test.js
@@ -14,6 +14,7 @@ describe('Module messageUtil', () => {
   afterEach( async () => {
     await dbClient.deinit();
   });
+  //
 
   it('Check mongoDB connection', () => {
     const status = dbClient.isConnected();
