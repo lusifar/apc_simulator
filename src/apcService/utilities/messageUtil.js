20b23412a0e63c0093ff37c9dda0f982caee2ab9
Azetry <azetry.bt10@nycu.edu.com>
2022-05-30 22:10:45 +0800

    replace global.cache into cacheParams(mongoDB)

--- src/apcService/utilities/messageUtil.js
+++ src/apcService/utilities/messageUtil.js
@@ -1,19 +1,37 @@
 const logger = require('../../utilities/logger')('APC_SERVICE');
+const cacheParams = require('../../controllers/params');
 
-const natsMessageHandler = (message) => {
-  if (!global.cache) {
+const natsMessageHandler = async (message) => {
+  // if (!global.cache) {
+  //   return;
+  // }
+
+  try {
+    //
+    const data = await cacheParams.get({});
+  } catch (err) {
+    //
     return;
   }
 
+
   const msgObj = JSON.parse(message);
   if (msgObj.type === 'FACTOR_THICKNESS') {
-    global.cache.set('FACTOR_THICKNESS', msgObj.factor);
+    // global.cache.set('FACTOR_THICKNESS', msgObj.factor);
+    const result = await cacheParams.update({}, {factor_thickness: msgObj.factor});
+    if (result.ok) 
+      logger.info(`receive thickness factor: ${msgObj.factor}`);
+    else 
+      logger.err(`not receive thickness factor: ${msgObj.factor}`);
 
-    logger.info(`receive thickness factor: ${msgObj.factor}`);
   } else if (msgObj.type === 'FACTOR_MOISTURE') {
-    global.cache.set('FACTOR_MOISTURE', msgObj.factor);
+    // global.cache.set('FACTOR_MOISTURE', msgObj.factor);
+    const result = await cacheParams.update({}, {factor_moisture: msgObj.factor});
+    if (result.ok) 
+      logger.info(`receive moisture factor: ${msgObj.factor}`);
+    else 
+      logger.err(`not receive moisture factor: ${msgObj.factor}`);
 
-    logger.info(`receive moisture factor: ${msgObj.factor}`);
   }
 };
 
