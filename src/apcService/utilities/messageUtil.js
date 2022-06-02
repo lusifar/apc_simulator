c7d42278a0ee5fd3d311b33ec9d35935c3b55904
KuangDW <71681113+KuangDW@users.noreply.github.com>
2022-06-02 23:11:05 +0800

    Update messageUtil.js

--- src/apcService/utilities/messageUtil.js
+++ src/apcService/utilities/messageUtil.js
@@ -1,17 +1,17 @@
 const logger = require('../../utilities/logger')('APC_SERVICE');
 
 const natsMessageHandler = (message) => {
-  if (!global.cache) {
+  if (!global.mongoCache) {
     return;
   }
 
   const msgObj = JSON.parse(message);
   if (msgObj.type === 'FACTOR_THICKNESS') {
-    global.cache.set('FACTOR_THICKNESS', msgObj.factor);
+    global.mongoCache.set('FACTOR_THICKNESS', msgObj.factor);
 
     logger.info(`receive thickness factor: ${msgObj.factor}`);
   } else if (msgObj.type === 'FACTOR_MOISTURE') {
-    global.cache.set('FACTOR_MOISTURE', msgObj.factor);
+    global.mongoCache.set('FACTOR_MOISTURE', msgObj.factor);
 
     logger.info(`receive moisture factor: ${msgObj.factor}`);
   }
