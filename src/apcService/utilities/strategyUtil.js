2b9709df3cb1e95a12edfa9719cffb5c825b3481
buck5712 <104512304+buck5712@users.noreply.github.com>
2022-06-13 13:35:16 +0800

    Update strategyUtil.js

--- src/apcService/utilities/strategyUtil.js
+++ src/apcService/utilities/strategyUtil.js
@@ -1,18 +1,42 @@
 const defaultStrategy = (moisture, mFactor) => {
+  const period = (moisture * mFactor).toFixed(2);
+
   return {
-    period: (moisture * mFactor).toFixed(2),
+    period,
     temperature: 100,
   };
 };
 
 const sharonStrategy = (thickness, tFactor) => {
+  const temperature = (thickness * tFactor).toFixed(2);
+
   return {
     period: 20,
-    temperature: (thickness * tFactor).toFixed(2),
+    temperature,
+  };
+};
+
+const ribeyeStrategy = (thickness, tFactor) => {
+  const temperature = (thickness * tFactor).toFixed(2);
+
+  return {
+    period: 22,
+    temperature,
+  };
+};
+
+const newyorkStrategy = (thickness, tFactor) => {
+  const temperature = (thickness * tFactor).toFixed(2);
+
+  return {
+    period: 25,
+    temperature,
   };
 };
 
 module.exports = {
   defaultStrategy,
   sharonStrategy,
+  ribeyeStrategy,
+  newyorkStrategy,
 };
